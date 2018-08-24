46871ea8e5d1423f8a04655b501d23f2c439da87
Nafies Luthfi <nafiesl@gmail.com>
2018-08-25 07:50:17 +0800

    Add LoginTest

---
+++ tests/Feature/Auth/LoginTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Tests\Feature\Auth;
+
+use App\User;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class LoginTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /** @test */
+    public function user_can_login_and_logout()
+    {
+        $user = factory(User::class)->create(['name' => 'Nama Member', 'email' => 'email@mail.com']);
+
+        $this->visit(route('login'));
+
+        $this->submitForm(__('Login'), [
+            'email'    => 'email@mail.com',
+            'password' => 'secret',
+        ]);
+
+        $this->seeText('You are logged in!');
+        $this->seePageIs(route('home'));
+        $this->seeIsAuthenticated();
+
+        $this->post(route('logout'));
+
+        $this->dontSeeIsAuthenticated();
+    }
+
+    /** @test */
+    public function user_invalid_login()
+    {
+        $this->visit(route('login'));
+
+        $this->submitForm(__('Login'), [
+            'email'    => 'email@mail.com',
+            'password' => 'member',
+        ]);
+
+        $this->seePageIs(route('login'));
+        $this->dontSeeIsAuthenticated();
+    }
+
+    /** @test */
+    public function unauthenticated_users_are_redirects_to_login_page()
+    {
+        $this->visit(route('home'));
+        $this->seePageIs(route('login'));
+    }
+}
