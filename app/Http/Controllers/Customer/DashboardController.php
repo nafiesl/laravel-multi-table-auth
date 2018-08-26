b0e585210791c269ba013965065cd365e4d53f7c
Nafies Luthfi <nafiesl@gmail.com>
2018-08-26 09:43:13 +0800

    Add docblock on customer dashboard controller

--- app/Http/Controllers/Customer/DashboardController.php
+++ app/Http/Controllers/Customer/DashboardController.php
@@ -6,11 +6,22 @@ use App\Http\Controllers\Controller;
 
 class DashboardController extends Controller
 {
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+
     public function __construct()
     {
         $this->middleware('auth:customer');
     }
 
+    /**
+     * Show the customer dashboard.
+     *
+     * @return \Illuminate\View\View
+     */
     public function index()
     {
         return view('customer_home');
